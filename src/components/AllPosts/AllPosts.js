7b674f87bfb109281a43138418c709cd0fef1329
Nur A Alam <nuraalam376@gmail.com>
2021-05-30 13:08:34 +0600

    implemented all posts using jsonplaceholder api

--- src/components/AllPosts/AllPosts.js
+++ src/components/AllPosts/AllPosts.js
@@ -1,9 +1,32 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Button } from "react-bootstrap";
 
 function AllPosts() {
+  const [posts, setPosts] = useState([]);
+  const [numberOfPosts, setNumberOfPosts] = useState(10);
+
+  const getAllPosts = async () => {
+    const response = await axios.get(
+      "https://jsonplaceholder.typicode.com/posts"
+    );
+    setPosts(response.data.splice(posts.length, numberOfPosts));
+  };
+
+  useEffect(() => {
+    getAllPosts();
+  }, [numberOfPosts]);
+
+  const loadMorePosts = () => {
+    setNumberOfPosts(posts.length + 10);
+  };
+
   return (
     <div>
       <h1>All Posts</h1>
+      <Button variant="info" onClick={loadMorePosts}>
+        Load More
+      </Button>
     </div>
   );
 }
