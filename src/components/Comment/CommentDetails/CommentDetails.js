e9cb7dcd5744051803824567d57a4d02ae6014d1
Nur A Alam <nuraalam376@gmail.com>
2021-06-01 12:56:43 +0600

    added icons

--- src/components/Comment/CommentDetails/CommentDetails.js
+++ src/components/Comment/CommentDetails/CommentDetails.js
@@ -1,12 +1,21 @@
 import React from "react";
+import { ChatLeftDotsFill, Mailbox, Person } from "react-bootstrap-icons";
 
 function CommentDetails({ comment }) {
   const { name, email, body } = comment;
   return (
     <div className="w-75 m-4 text-left">
-      <h5>{name}</h5>
-      <h6>{email}</h6>
-      <p>{body}</p>
+      <h5>
+        <Person />
+        {name}
+      </h5>
+      <h6>
+        <Mailbox />
+        {email}
+      </h6>
+      <p>
+        <ChatLeftDotsFill /> {body}
+      </p>
     </div>
   );
 }
