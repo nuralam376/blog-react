64c3f3ef40f42487d177e8185c7d6b2ea6954346
Nur A Alam <nuraalam376@gmail.com>
2021-06-01 13:03:44 +0600

    changed text color

--- src/components/Comment/CommentDetails/CommentDetails.js
+++ src/components/Comment/CommentDetails/CommentDetails.js
@@ -7,7 +7,7 @@ function CommentDetails({ comment }) {
     <div className="w-75 m-4 text-left">
       <h5>
         <Mailbox />
-        {name}
+        <span className="text-info ml-1">{name}</span>
       </h5>
       <h6>
         <Person />
